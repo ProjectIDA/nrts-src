7e3ce87553de5f29965871961ede79f98eee77ac
danauerbach <dauerbach@ucsd.edu>
2018-01-05 01:04:14 +0000

    reorg to init q330 structs only after entire cmdline is processed. This allows site name to appear anywhere on cmdline

--- bin/isi330/init.c
+++ bin/isi330/init.c
@@ -26,24 +26,6 @@ void help(char *myname)
     exit(1);
 }
 
-static char *prefix(ISI330_CONFIG *cfg)
-{
-    char *src, *string;
-
-    src = cfg->site;
-
-    if ((string = strdup(src)) == NULL) {
-        perror("strdup");
-        exit(MY_MOD_ID);
-    } else {
-        util_ucase(string);
-    }
-
-    return string;
-}
-
-
-
 ISI330_CONFIG *init(char *myname, int argc, char **argv)
 {
     static char *fid = "init";
@@ -54,6 +36,9 @@ ISI330_CONFIG *init(char *myname, int argc, char **argv)
     char *user = ISI330_DEFAULT_USER;
     char *cfgpath = NULL;
     int depth = DEFAULT_PACKET_QUEUE_DEPTH;
+    Q330_CFG *q330cfg = NULL;  /* q330 database info */
+    int errcode;
+    LNKLST q330Hosts;
 
 
     if ((cfg = (ISI330_CONFIG *) malloc(sizeof(ISI330_CONFIG))) == NULL) {
@@ -63,54 +48,82 @@ ISI330_CONFIG *init(char *myname, int argc, char **argv)
     cfg->site = NULL;
     cfg->port = -1;
 
-/*  Get command line arguments  */
+    listInit(&q330Hosts);
 
-     listInit(&cfg->q330list);
+/*  Get command line arguments  */
 
     for (int i = 1; i < argc; i++) {
         printf("arg %d of %d: %s\n", i, argc, argv[i]);
 
         if (strncmp(argv[i], "q330=", strlen("q330=")) == 0) {
-            if ((cfg->cfgpath = AddQ330(cfg, argv[i] + strlen("q330="), cfgpath)) == NULL) {
-                fprintf(stderr, "%s: failed to add Q330: %s\n", myname, strerror(errno));
-                exit(MY_MOD_ID);
+
+            if (!listAppend(&q330Hosts, argv[i] + strlen("q330="), (UINT32)(strlen(argv[i]) - strlen("q330=") + 1))) {
+                fprintf(stderr, "%s: listAppend: %s for string: %s\n", fid, strerror(errno), argv[i] + strlen("q330="));
+                exit(MY_MOD_ID + 2);
             }
+
         } else if (strncmp(argv[i], "cfg=", strlen("cfg=")) == 0) {
+
              if (cfgpath != NULL) {
                  fprintf(stderr, "ERROR: multilple instances of cfg argument are not allowed\n");
-                 exit(MY_MOD_ID);
+                 exit(MY_MOD_ID + 3);
              }
              cfgpath = argv[i] + strlen("cfg=");
+
         } else if (strncasecmp(argv[i], "dl=", strlen("dl=")) == 0) {
+
             if (!utilParseServer(argv[i]+strlen("dl="), cfg->server, &cfg->port)) {
                 fprintf(stderr, "error parsing dl argument: %s\n", strerror(errno));
-                exit(1);
+                exit(MY_MOD_ID + 4);
             }
+
         } else if (strncmp(argv[i], "net=", strlen("net=")) == 0) {
+
             strcpy(cfg->netname, argv[i] + strlen("net="));
+
         } else if (strncmp(argv[i], "log=", strlen("log=")) == 0) {
+
             log = argv[i] + strlen("log=");
+
         } else if (strcmp(argv[i], "-bd") == 0) {
+
             daemon = TRUE;
+
         } else if (strcmp(argv[i], "-debug") == 0) {
+
             debug = TRUE;
+
         } else if (cfg->site == NULL) {  // this must be last
+
             if ((cfg->site = strdup(argv[i])) == NULL) {
                 fprintf(stderr, "%s: strdup: %s\n", myname, strerror(errno));
-                exit(MY_MOD_ID);
+                exit(MY_MOD_ID + 5);
             }
+
         } else {
             fprintf(stderr, "%s: unrecognized argument '%s'\n", myname, argv[i]);
             help(myname);
         }
     }
 
-/* make list of Q330s */
+    /* make list of Q330s */
 
-     if (!listSetArrayView(&cfg->q330list)) {
-         fprintf(stderr, "%s: listSetArrayView: %s", myname, strerror(errno));
-         exit(MY_MOD_ID);
-     }
+    if (!listSetArrayView(&cfg->q330list)) {
+        fprintf(stderr, "%s: listSetArrayView: %s", myname, strerror(errno));
+        exit(MY_MOD_ID + 6);
+    }
+
+    /* Read ida q330 database */
+
+    if ((q330cfg = q330ReadCfg(cfgpath, &errcode)) == NULL) {
+        q330PrintErrcode(stderr, "q330ReadCfg: ", cfgpath, errcode);
+        exit(MY_MOD_ID + 7);
+    }
+
+    /* Get DB info for each q330 specified on cmd line
+     * and set up initialization structures for each */
+
+    LoadQ330Hosts(cfg, &q330Hosts, q330cfg);
 
 /* Must specify site name */
 
@@ -126,15 +139,15 @@ ISI330_CONFIG *init(char *myname, int argc, char **argv)
 
      if (daemon && !BackGround(cfg)) {
          perror("BackGround");
-         exit(MY_MOD_ID);
+         exit(MY_MOD_ID + 8);
      }
 
 /* Start logging facility */
 
      if (log == NULL) log = daemon ? DEFAULT_BACKGROUND_LOG : DEFAULT_FOREGROUND_LOG;
-     if ((cfg->lp = InitLogging(myname, log, prefix(cfg), debug)) == NULL) {
+     if ((cfg->lp = InitLogging(myname, log, util_ucase(cfg->site), debug)) == NULL) {
          perror("InitLogging");
-         exit(MY_MOD_ID);
+         exit(MY_MOD_ID + 9);
      }
 
 /* Start signal handling thread */
@@ -163,7 +176,7 @@ ISI330_CONFIG *init(char *myname, int argc, char **argv)
     // start Q330 readers
     StartQ330Readers(cfg);
 
-    LogMsg("%s: initialization complete", fid);
+    LogMsg("%s: initialization complete", util_ucase(myname));
 
     return cfg;
 }
