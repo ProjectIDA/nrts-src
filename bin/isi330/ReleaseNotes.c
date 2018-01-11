8dda7bb1e6a62c7b04b80e3e13feae35ed24e245
danauerbach <dauerbach@ucsd.edu>
2018-01-11 23:07:21 +0000

    add comment string. duh

--- bin/isi330/ReleaseNotes.c
+++ bin/isi330/ReleaseNotes.c
@@ -1,8 +1,13 @@
 char *VersionIdentString = "Release 0.9.0";
 
 /* Release Notes */
-0.9.0   dauerbach
+/*
+ *
+0.9.0   2018-01-11 dauerbach
         Only allow single q330 host
         Change DP to use 1-based indexing
         Change DATA and CTRL ports to use q330->addr instance offset
             so each q330 has unique ports across multiple instances
+
+
+*/
