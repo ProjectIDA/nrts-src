d1b74754c9d9f375c69ccb7465d0ec1cb60c0790
danauerbach <dauerbach@ucsd.edu>
2018-03-07 11:35:31 -0800

    podir version 1.1.3 release notes

--- bin/podir/ReleaseNotes.c
+++ bin/podir/ReleaseNotes.c
@@ -1,11 +1,14 @@
 #pragma ident "$Id: ReleaseNotes.c,v 1.9 2017/04/14 19:38:19 dechavez Exp $"
 
-char *VersionIdentString = "Release 1.1.2";
+char *VersionIdentString = "Release 1.1.3";
 
 /* Release notes
 
+1.1.3 02/21/2018 dauerbach
+      Fresh build with Antelope 5.7 libraries
+
 1.1.2 03/02/2017
-      Fresh build with Antelope 2.6 libraries
+      Fresh build with Antelope 5.6 libraries
 
 1.1.1 12/02/2015
       Link with libfilter 1.2.6 so that things won't fall apart uneccesarily if there
