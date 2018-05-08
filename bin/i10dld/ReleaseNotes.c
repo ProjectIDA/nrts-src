dcaddbc59354472ecf2b56e2aa248ae2ce731069
David Chavez <davidechavez@gmail.com>
2018-05-08 09:24:57 -0700

    link with libisidb 1.5.0 to not require revs file for rev=10 systems

--- bin/i10dld/ReleaseNotes.c
+++ bin/i10dld/ReleaseNotes.c
@@ -1,8 +1,11 @@
 #pragma ident "$Id: ReleaseNotes.c,v 1.3 2018/01/11 19:07:32 dechavez Exp $"
 
-char *VersionIdentString = "Release 1.1.0";
+char *VersionIdentString = "Release 1.1.1";
 
 /* Release notes
+
+1.1.1  05/08/2018
+       link with libisidb 1.5.0 to not require revs file for rev=10 systems
 
 1.1.0  05/07/2018
        link with libdbio 3.5.1 to ignore missing deprecated files in ~nrts/etc
